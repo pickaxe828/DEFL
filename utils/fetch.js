3d855080fdc5d691a042f70ce7a3f2549bfb0578
Pickaxe828 <joshuajcn828@gmail.com>
2022-03-21 01:57:15 +0800

    Change directory

--- utils/fetch.js
+++
@@ -1,8 +0,0 @@
-let proxy_url = "https://api.allorigins.win/get?url="
-// "https://worldpopulationreview.com/"
-
-export async function fetchProxied(url) {
-    let response = await fetch(proxy_url + url)
-    let data = await response.json()
-    return data
-}
